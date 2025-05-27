bb432700371f9bf03a0efc5342c1d6c57dedccac
dima <nubitrwgt@gmail.com>
2025-05-28 01:13:48 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'dumitrw-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
